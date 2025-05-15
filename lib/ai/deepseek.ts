195042c3e883741d707d35c17ef2cd91912cdc7e
Snakin <70094531+Snakinya@users.noreply.github.com>
2025-05-15 16:08:44 +0800

    fix

--- lib/ai/deepseek.ts
+++ lib/ai/deepseek.ts
@@ -3,8 +3,5 @@ import { createDeepSeek } from '@ai-sdk/deepseek';
 export const deepseek = createDeepSeek({
   apiKey: process.env.DEEPSEE_API_KEY ?? '',
   baseURL: 'https://api.lkeap.tencentcloud.com/v1',
-  headers: {
-    'Authorization': `Bearer ${process.env.DEEPSEE_API_KEY}`,
-    
-  },
+
 }); 
